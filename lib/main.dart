ae6815d5c6d91762da577be1eef1a8ca4cf30554
Abdelrahman Ashraf <abdelrahman1711335@miuegypt.edu.eg>
2021-01-26 17:03:55 +0200

    Admin showroom GUI 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:car_qr/Providers/auth.dart';
 import 'package:car_qr/Providers/available_cars_model.dart';
 import 'package:car_qr/Screens/about.dart';
+import 'package:car_qr/Screens/addshowroom.dart';
+import 'package:car_qr/Screens/carlist.dart';
 import 'package:car_qr/Screens/carshowroom.dart';
 import 'package:car_qr/Screens/myappbar.dart';
 import 'package:car_qr/Screens/history.dart';
 import 'package:car_qr/Screens/signin.dart';
 import 'package:car_qr/Screens/settings.dart';
+import 'package:car_qr/Screens/user_car_review.dart';
 import 'package:flutter/material.dart';
 import 'package:car_qr/Screens/car_description.dart';
 import 'package:flutter/services.dart';
@@ -19,8 +22,10 @@ import 'package:provider/provider.dart';
 import 'package:car_qr/Providers/auth.dart';
 import 'package:car_qr/Models/user.dart';
 
+import 'Screens/carshowroomadmin.dart';
+
 void main() {
-  runApp(MyApp2());
+  runApp(Carshowroomadmin());
 }
 
 class MyApp2 extends StatelessWidget {
@@ -28,7 +33,7 @@ class MyApp2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
-        child: MaterialApp(
+      child: MaterialApp(
         home: Wrapper(),
       ),
     );
